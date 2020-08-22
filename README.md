0abec822885c9e41cf44086e8e9b186f12f10d10
Thalia <thalia.rowan@gmail.com>
2020-08-21 23:50:46 -0300

    Placed photos in seperate folder .

--- README.md
+++ README.md
@@ -4,14 +4,14 @@
 
 For this assignment, I was tasked to write javascript so that when the Generate Password button is clicked, it outputs random characters. When the button is clicked, a question is prompted for the user to input the length they want their password to be.
 
-![](pg.png)
+![](./assets/pg.png)
 
 When the number is inputed, it goes on to ask what characters would they would or would not like to include, example includes
 
-![](pg2.png);
+![](./assets/pg2.png)
 
 This goes on until the last confirm, then password is generated
 
-![](final.png)
+![](./assets/final.png)
 
-Link to the https://thaliarowan.github.io/homwork3.github.io/
+Link to page https://thaliarowan.github.io/homwork3.github.io/
--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lower = ('abcdefghijklmnopqrstuvwxyz').split("");
- var upper = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ').split("");
- var specialChar = [ '!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~',]
- var num = [1,2,3,4,5,6,7,8,9];
- var myArr = [];
- 
-
-
+var lower = "abcdefghijklmnopqrstuvwxyz".split("");
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var specialChar = [
+  "!",
+  '"',
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var myArr = [];
 
 // Write password to the #password input
 function writePassword() {
@@ -15,66 +44,63 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-function generatePassword(){
-  
-var count = prompt("How long do you want your password? Must be at least 6 but not more than 128");
-var lowerCase = confirm("Do you want LowerCase characters?");
-var upperCase = confirm("Do you want UpperCase characters?");
-var special = confirm("Do you want special characters?");
-var number = confirm("Do you want to add numbers?");
-  let result = '';
+function generatePassword() {
+  var count = prompt(
+    "How long do you want your password? Must be at least 6 but not more than 128"
+  );
+  var lowerCase = confirm("Do you want LowerCase characters?");
+  var upperCase = confirm("Do you want UpperCase characters?");
+  var special = confirm("Do you want special characters?");
+  var number = confirm("Do you want to add numbers?");
+  let result = "";
   let myArr = [];
- 
-  if(lowerCase === true){
-    myArr.push(lower)
-  }; 
 
-  if(upperCase === true){
-    myArr.push(upper)
-  }; 
-  
-  if(special === true){
-    myArr.push(specialChar)
-  };
+  if (lowerCase === true) {
+    myArr.push(lower);
+  }
 
-  if(number === true){
-    myArr.push(num)
-  }; 
+  if (upperCase === true) {
+    myArr.push(upper);
+  }
+
+  if (special === true) {
+    myArr.push(specialChar);
+  }
+
+  if (number === true) {
+    myArr.push(num);
+  }
 
   let newArr = myArr.flat();
 
-  for(let i = 0; i <= count-1; i++){
-    var arrIdx = Math.floor(Math.random()* newArr.length);
-    var arrRand = newArr[arrIdx]
+  for (let i = 0; i <= count - 1; i++) {
+    var arrIdx = Math.floor(Math.random() * newArr.length);
+    var arrRand = newArr[arrIdx];
     result += arrRand;
-
-  }  
-  
-  if( newArr.length === 0){
-    alert('You have not chosen any characters to include');
-    return '';
   }
 
-  if(count < 8){
-     alert("Your length must have at least 8 characters")
-     return '';
-  } else if(count > 128){
-    alert("Your length must not excede 128 characters")
-    return '';
+  if(typeof count !== Number){
+    alert(" Your imput was not a number, Please input a number");
+    return "";
+  }
+
+  if (newArr.length === 0) {
+    alert("You have not chosen any characters to include");
+    return "";
+  }
+
+  if (count < 8) {
+    alert("Your length must have at least 8 characters");
+    return "";
+  } else if (count > 128) {
+    alert("Your length must not excede 128 characters");
+    return "";
   } else {
     return result;
   }
-  
 }
-
-
-
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
